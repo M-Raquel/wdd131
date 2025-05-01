8d02b1dccd2cbef7bc20767be69f6f01829a7581
M-Raquel <mil22025@byui.edu>
2025-05-01 13:16:02 -0600

    added the other image

--- mission/mission.js
+++ mission/mission.js
@@ -11,12 +11,13 @@ function changeTheme() {
         //change body to dark
         document.body.className = 'dark';
         //change logo to the new logo
-        
+        logo.src="byui-logo_white.png";
     }
     else 
     {
         //change body to light OR remove dark class from body
         document.body.classList.remove('dark');
         //change logo back to original ; change the src 
+        logo.src="byui-logo_blue.webp";
     }
 }
