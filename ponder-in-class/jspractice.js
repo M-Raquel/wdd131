be8c1cdd0da668c408dc1e488c8acc5d4f6677be
M-Raquel <mil22025@byui.edu>
2025-05-01 15:05:25 -0600

    Practices we do in class

---
+++ ponder-in-class/jspractice.js
@@ -0,0 +1,15 @@
+//JavaScript is case sensitive - usually have upper case variable names
+const PI = 3.14; //This is constant, won't change
+
+let raduis = 3;
+
+let area = radius * radius * PI;
+
+console.log(area);
+
+const one = 1;
+const two= "2";
+
+let result = one * two //incorrect practice
+console.log(result);
+
---
+++ ponder-in-class/target.js
@@ -0,0 +1,11 @@
+const element = document.querySelector("#intro");
+
+element.style.backgroundColor = "yellow";
+
+const voyager = document.querySelector("em")
+
+voyager.Text.Content = "USS Voyager Starship";
+
+let img = document.querySelector("#starship")
+
+
